b9e2a2d54663d6536c172e8ad016ab926dbde0f6
Mohamed Habib Agrebi <143283829+Engmhabib@users.noreply.github.com>
2025-03-15 02:42:50 -0400

    Update load_to_sql.py

--- atabricks_notebooks/load_to_sql.py
+++ atabricks_notebooks/load_to_sql.py
@@ -5,13 +5,7 @@ jdbc_properties = {
     "password": "your_password",
     "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
 }
-
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
 df = spark.read.table('processed_transactions')
-
-(df.write
-   .format("jdbc")
-   .mode("overwrite")
-   .option("url", jdbc_url)
-   .option("dbtable", jdbc_table)
-   .options(**jdbc_properties)
-   .save())
+df.write.format("jdbc").mode("overwrite").option("url", jdbc_url).option("dbtable", jdbc_table).options(**jdbc_properties).save()
