cf34babf336251e24b8291073aea972bc9a8a710
Mohamed Habib Agrebi <143283829+Engmhabib@users.noreply.github.com>
2025-03-15 02:36:01 -0400

    Create load_to_sql.py

---
+++ atabricks_notebooks/load_to_sql.py
@@ -0,0 +1,17 @@
+jdbc_url = "jdbc:sqlserver://your_sql_server.database.windows.net:1433;database=customerDB"
+jdbc_table = "dbo.customer_transactions"
+jdbc_properties = {
+    "user": "sql_admin",
+    "password": "your_password",
+    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+}
+
+df = spark.read.table('processed_transactions')
+
+(df.write
+   .format("jdbc")
+   .mode("overwrite")
+   .option("url", jdbc_url)
+   .option("dbtable", jdbc_table)
+   .options(**jdbc_properties)
+   .save())
